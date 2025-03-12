5528c8949061a85f1ca07c045ec1549dc717c314
MINDtestia <iaformation888@gmail.com>
2025-03-12 21:20:00 +0100

    Delete cache directory

--- cache/my_page/setting/integrations.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def show_integrations():
-    st.title("🔗 Intégrations externes")
-
-    if st.button("Connecter à Google Drive"):
-        st.write("Connexion à Drive à implémenter avec OAuth2.")
